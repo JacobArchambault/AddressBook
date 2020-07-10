755c72f0f8cf40af742e1df6ce41d33776e111de
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-09 22:08:29 -0400

    Add project files.

---
+++ Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Entry
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string PhoneNumber { get; set; }
+    }
+}
