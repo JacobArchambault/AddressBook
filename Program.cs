c5b52c892ea8007c41488ce0650b14ac71bfbc68
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-11 13:07:37 -0400

    Achieve working program writing to file.

--- Program.cs
+++ Program.cs
@@ -5,13 +5,26 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text.RegularExpressions;
 using System.Transactions;
 using static System.Console;
+using System.IO;
+using System.Diagnostics;
+
 namespace AddressBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            AddEntries();
+            List<Entry> entries = AddEntries();
+            using (StreamWriter writer = File.CreateText("phone-book.txt"))
+            {
+                foreach (Entry entry in entries)
+                {
+                    int entryNumber = entries.IndexOf(entry);
+                    writer.WriteLine($"Entry {entryNumber + 1}:\tName: {entry.Name},\tAddress: {entry.Address},\tphone number: {entry.PhoneNumber}");
+                }
+            }
+            Process.Start("notepad.exe", "phone-book.txt");
+
         }
 
         static List<Entry> AddEntries()
@@ -29,11 +42,11 @@ namespace AddressBook
             WriteLine("Press enter 1 to add another entry. Enter any other key to exit");
             return ReadLine();
         }
+
         static Entry AddNewEntry()
         {
             return new Entry { Name = GetFieldFromUser("name"), Address = GetFieldFromUser("address"), PhoneNumber = GetPhoneNumber() };
         }
-
         static string GetFieldFromUser(string desiredField)
         {
             WriteLine($"Enter your {desiredField}: ");
@@ -48,6 +61,7 @@ namespace AddressBook
             }
             return phoneNumber;
         }
+
         static bool PhoneNumberIsValidFormat(out string phoneNumber)
         {
             Regex regex = new Regex(@"\d{3}-\d{3}-\d{4}");
